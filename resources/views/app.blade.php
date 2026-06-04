0fd6080bc2e8d387628e359efc8df6e459455928
thomasandrianto <thomasandrianto1980@gmail.com>
2026-06-04 12:09:23 +0700

    fix: add viewport meta tag for mobile responsiveness

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,6 +2,12 @@
 <html lang="id">
 <head>
     <meta charset="UTF-8">
+
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0"
+    >
+
     <title>Web Sekolah</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
